f785e7c4a9ddbf7e536094920c9c5953e8492d93
Luca Saiu <saiu@lipn.univ-paris13.fr>
2008-04-07 15:05:51 +0200

    Configuration file support: finished, cleaned, commented

--- CONFIGURATION/configuration_files.ml
+++ CONFIGURATION/configuration_files.ml
@@ -19,34 +19,33 @@ open PreludeExtra.Prelude;; (* We want synchronous terminal output *)
 open UnixExtra;;
 open StringExtra;;
 
-(* To do: move this into UnixExtra or something like that: *)
-(** Run system with the given argument, and raise exception in case of failure;
-    return unit on success. *)
-let system_or_fail command_line =
-  Printf.printf "Executing \'%s\'...\n" command_line;
-  flush_all ();
-  match Unix.system command_line with
-    Unix.WEXITED 0 ->
-      ()
-  | Unix.WEXITED n ->
-      failwith (Printf.sprintf "Unix.system: the process exited with %i" n)
-  | Unix.WSIGNALED _ | Unix.WSTOPPED _ ->
-      failwith "Unix.system: the process was signaled or stopped";;
+(** An alist is just a list of pairs: *)
+type 'a alist =
+    (string * 'a) list;;
 
+(** For each variable bound in the shell environment echo its name and its value,
+one binding per line: *)
 let output_of_environment variables =
   let command_line =
     List.fold_left
       (fun string variable ->
-        (* Print a line with: the variable name, a space, and its value *)
-(** To do: this must be changed so that $%s is only echo'ed if it's defined. *)
-        Printf.sprintf "%s echo %s $%s; " string variable variable)
+        (* Print a line with: the variable name, a space, and its value,
+           IF the variable is defined in the environment; otherwise don't
+           print anything, and let the configuration file binding (if any)
+           take precedence: *)
+        Printf.sprintf
+          "%s if test -n \"$%s\"; then echo %s \"$%s\"; fi; "
+          string variable variable variable)
       ""
       variables in
+  (* Printf.printf "The command line is\n%s\n" command_line; *)
   let (output, exit_code) = Unix.run command_line in
   assert(exit_code = Unix.WEXITED 0);
-  Printf.printf "The output is:\n-------------------------\n%s\n-------------------------\n" output;
+  (* Printf.printf "The output is:\n-------------------------\n%s\n-------------------------\n" output; *)
   output;;
 
+(** Evaluate the given file, then 'echo' each variable name and its value, one variable
+    per line: *)
 let output_of_file_name file_name variables =
   let source_command_line =
     Printf.sprintf "set -e; (source %s &&" file_name in
@@ -58,7 +57,7 @@ let output_of_file_name file_name variables =
       source_command_line
       variables in
   let command_line = command_line ^ " true)" in
-  Printf.printf "The command line is %s\n" command_line;
+  (* Printf.printf "The command line is %s\n" command_line; *)
   let (output, exit_code) = Unix.run command_line in
   if not (exit_code = Unix.WEXITED 0) then
     failwith ("Failed when source'ing the configuration file " ^ file_name)
@@ -67,51 +66,75 @@ let output_of_file_name file_name variables =
     output;
   end;;
 
+(** Convert an output into a list of rows, where each row is a list of
+    strings: first the variable name, then the value, possibly made of several
+    tokens: *)
 let matrix_of_output output =
   String.Text.Matrix.of_string output;;
 
+(** Extract only the variable names from the matrix, disregarding values: *)
 let variables_of_matrix matrix =
   List.map
     (fun row -> match row with
-                | variable :: _ -> variable
-                | _ -> assert false)
+    | variable :: _ -> variable
+    | _ -> assert false) (* no line should be empty *)
     matrix;;
 
+(** Turn a matrix into an alist mapping each variable name into a value; each 
+    variable value (as a list of strings) is passed to the given function to
+    obtain the value which is bound in the returned environment. Variables for
+    which the given function fails are simply ignored: *)
 let alist_of_matrix row_to_element matrix =
   let result = ref [] in
   List.iter
     (fun row ->
       match row with
-      | (variable :: values) -> begin
-          try
-            result := (variable, (row_to_element values)) :: !result;
-          with _ ->
-            ()
-        end
+      | (variable :: values) ->
+          (try result := (variable, (row_to_element values)) :: !result; with _ -> ())
       | _ ->
           assert false)
     matrix;
   !result;;
 
-let scalar_alist_of_matrix string_to_element=
+(** Turn a matrix into an alist mapping each variable name into a value; each 
+    variable value (as a single string, with token separated by a single space)
+    is passed to the given function to obtain the value which is bound in the
+    returned environment. Variables for which the given function fails are simply
+    ignored: *)
+let scalar_alist_of_matrix string_to_element =
   alist_of_matrix
     (fun values -> string_to_element (String.Fold.blankcat values));;
 
+(** Turn a matrix into an alist mapping each variable name into the list of
+    the tokens of its value: *)
 let list_alist_of_matrix =
   alist_of_matrix (fun values -> values);;
 
+(** Turn a matrix into an alist mapping each variable name into the string
+    containing its value (tokens concatenated into a string, separated by
+    single spaces): *)
 let string_alist_of_matrix =
   scalar_alist_of_matrix (fun string -> string);;
 
+(** Turn a matrix into an alist mapping each variable name with an integer
+    value into the integer. Non-integer-valued variables are ignored: *)
 let int_alist_of_matrix =
   scalar_alist_of_matrix int_of_string;;
 
+(** Turn a matrix into an alist mapping each variable name with an float
+    value into the float. Non-float-valued variables are ignored: *)
 let float_alist_of_matrix =
   scalar_alist_of_matrix float_of_string;;
 
+(** Turn a matrix into an alist mapping each variable name with an bool
+    value into the bool. Non-bool-valued variables are ignored: *)
 let bool_alist_of_matrix =
   scalar_alist_of_matrix bool_of_string;;
 
+(** Turn a matrix into a "tuple of alists", which henceforth means
+    an alist of strings, an alist of ints, an alist of floats, an
+    alist of bools, and an alist of lists of strings; as usual, values
+    of the "wrong" type are ignored: *)
 let alists_of_output output variables =
   let matrix = matrix_of_output output in
   let string_alist : (string * string) list =
@@ -125,10 +148,11 @@ let alists_of_output output variables =
   let list_alist : (string * (string list)) list =
     list_alist_of_matrix matrix in
   string_alist, int_alist, float_alist, bool_alist, list_alist;;
-  
-let alists_of_file file_name variables =
-  let output = output_of_file_name file_name variables in
-  alists_of_output output variables;;
+                        
+                        (** Turn a *file* into a tuple of alists: *)
+                        let alists_of_file file_name variables =
+                          let output = output_of_file_name file_name variables in
+                          alists_of_output output variables;;
 
 (** Merge the two given alist groups; the latest one takes precedence: *)
 let merge_alists alists1 alists2 =
@@ -142,15 +166,27 @@ let merge_alists alists1 alists2 =
    bool_alist1 @ bool_alist2,
    list_alist1 @ list_alist2);;
 
-(* To do: autogenerate a meta.ml file from META in buildsystem. This allows us
-   to automatically get the package name (hence the configuration file name) 
-   without supplying ~file_names, which will become optional. *)
+(** Make a configuration object from a list of file name or a software name;
+    in the latter case the configuration files have "reasonable" default
+    names: *)
 class configuration =
-  fun ~file_names
+  fun ?software_name
+      ?file_names
       ~variables
       ?(read_environment=true)
       () ->
-object
+let file_names =
+  match file_names, software_name with
+  | None, None ->
+      failwith "either ~software_name or ~file_names should be passed"
+  | (Some file_names), None ->
+      file_names
+  | None, (Some software_name) ->
+      [ "/etc/" ^ software_name; "~/." ^ software_name ]
+  | (Some _), (Some _) ->
+      failwith "you should pass exactly one of ~software_name and ~file_names" in
+object(self)
+  (* Associative containers used for efficient access, after initialization: *)
   val string_hashmap = new Hashmap.hashmap ();
   val int_hashmap = new Hashmap.hashmap ();
   val float_hashmap = new Hashmap.hashmap ();
@@ -158,34 +194,53 @@ object
   val list_hashmap = new Hashmap.hashmap ();
 
   initializer
+    (* First execute all configuration files in the correct order, and merge the
+       bindings: *)
     let alists =
       List.fold_left
         (fun accumulator file_name ->
           merge_alists accumulator (alists_of_file file_name variables))
         ([], [], [], [], [])
         file_names in
+    (* If we also want to access the shell environment, then look it up, and
+       give it precedence over configuration files: *)
     let (string_alist, int_alist, float_alist, bool_alist, list_alist) =
       if read_environment then
         let environment_output = output_of_environment variables in
         merge_alists alists (alists_of_output environment_output variables)
       else
         alists in
+    (* Finally convert the bindings from alists into hashes, for efficient access: *)
     string_hashmap#add_alist string_alist;
     int_hashmap#add_alist int_alist;
     float_hashmap#add_alist float_alist;
     bool_hashmap#add_alist bool_alist;
     list_hashmap#add_alist list_alist;
-
+    
+  (** Lookup a variable of the given type: *)
   method lookup_string = string_hashmap#lookup
   method lookup_int = int_hashmap#lookup
   method lookup_float = float_hashmap#lookup
   method lookup_bool = bool_hashmap#lookup
   method lookup_list = list_hashmap#lookup
-end;;
+    
+  (** Aliases for accessors: *)
+  method string = self#lookup_string
+  method int = self#lookup_int
+  method float = self#lookup_float
+  method bool = self#lookup_bool
+  method list = self#lookup_list
+end;; (* class *)
 
-let q =
-  new configuration
-    ~file_names:["MYSETTINGS"]
-    ~variables:["ZZZ"]
-    ();;
-Printf.printf "%s\n" (q#lookup_string "ZZZ");;
+(* (\* Example *\) *)
+(* let q = *)
+(* new configuration *)
+(*   ~file_names:["~luca/working/ocamlbricks/MYSETTINGS"; "~luca/working/ocamlbricks/MYSETTINGS2"] *)
+(*   (\* ~software_name:"marionnet" *\) *)
+(*   ~variables:["ZZZ"; "fortytwo";] *)
+(*   ~read_environment:true *)
+(*   ();; *)
+
+(* Printf.printf ">%s<\n" (q#string "ZZZ");; *)
+(* Printf.printf ">%i<\n" (q#int "fortytwo");; *)
+(* Printf.printf ">%f<\n" (q#float "fortytwo");; *)
