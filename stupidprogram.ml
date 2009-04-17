3491425bb929bae1844da15a803ce697c5c8361c
Luca Saiu <saiu@lipn.univ-paris13.fr>
2009-04-17 18:10:26 +0200

    Added does_process_exist.c, a trivial C wrapper for kill called with signal 0

--- stupidprogram.ml
+++ stupidprogram.ml
@@ -1,2 +1,7 @@
 (* This is just a build system test. *)
 print_string "Hello, world!\n";;
+
+external does_process_exist : int -> bool = "does_process_exist_c";;
+
+let pid = 16523;;
+Printf.printf "Does the process with pid %i exist? %b\n" pid (does_process_exist pid);;
