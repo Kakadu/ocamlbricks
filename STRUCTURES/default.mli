01e60da277bb5ee1bd264e16d9868f2d80e7e572
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-05-11 10:13:55 +0200

    Added the module Default for managing optional parameters with global defaults

---
+++ STRUCTURES/default.mli
@@ -0,0 +1,33 @@
+(* This file is part of ocamlbricks
+   Copyright (C) 2010 Jean-Vincent Loddo
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
+(** Manage global defaults for your optional parameters. {b Example}:
+{[
+module Default_foo = Default.Make(struct type t = foo_type let create () = .. end)
+let my_function ?foo .. =
+ let foo = Default_foo.extract_or_get_default foo in
+ ...
+]}
+*)
+
+module Make :
+  functor (Value : sig type t val create : unit -> t end) ->
+    sig
+      type t = Value.t
+      val get : unit -> t
+      val set : t -> unit
+      val extract_or_get_default : t option -> t
+    end
