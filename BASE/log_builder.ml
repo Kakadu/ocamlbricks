a42b9e8ca4ae2a5f3c3988989ca6b86116f6c413
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-09-10 19:25:41 +0200

    Major changes in Log_builder. The printing functions have now the optional parameter ?v:int (verbosity), the log channel is now expressed by a polymorphic variant [ `stdout | `stderr | `file of string ], and the name of functions of the functor parameter are now (may be) quite intuitive: module Make : functor     (Tuning : sig            val verbosity    : int            val debug_level  : unit -> int            val log_channel  : log_channel            val synchronized : bool          end) -> Result

--- BASE/log_builder.ml
+++ BASE/log_builder.ml
@@ -24,30 +24,28 @@
 (** *)
 
 (** Data type representing the output channel where the messages will be written. *)
-type log_channel =
- | Stdout
- | Stderr
- | File of string
+type log_channel = [ `stdout | `stderr | `file of string ]
 
 (** The signature of the module resulting from functors' applications. *)
 module type Result = sig
-  val printf : ?force:bool -> ?banner:bool -> (('a, out_channel, unit) format) -> 'a
-  val print_exn     : ?force:bool -> ?banner:bool -> exn -> unit
-  val print_string  : ?force:bool -> string -> unit
-  val print_int     : ?force:bool -> int -> unit
-  val print_float   : ?force:bool -> float -> unit
-  val print_newline : ?force:bool -> unit -> unit
-  val print_endline : ?force:bool -> string -> unit
+  val printf        : ?v:int -> ?force:bool -> ?banner:bool -> (('a, out_channel, unit) format) -> 'a
+  val print_exn     : ?v:int -> ?force:bool -> ?banner:bool -> exn -> unit
+  val print_string  : ?v:int -> ?force:bool -> string -> unit
+  val print_int     : ?v:int -> ?force:bool -> int -> unit
+  val print_float   : ?v:int -> ?force:bool -> float -> unit
+  val print_newline : ?v:int -> ?force:bool -> unit -> unit
+  val print_endline : ?v:int -> ?force:bool -> string -> unit
 
   module Tuning:sig
-     val threshold             : unit -> int
-     val get_current_verbosity : unit -> int
-     val is_debug_enabled      : unit -> bool
+  
+     val verbosity             : unit -> int
+     val debug_level           : unit -> int
+     val is_log_enabled        : ?v:int -> unit -> bool
      val log_channel           : log_channel
      val synchronized          : bool
      module Set : sig
-       val threshold             : int -> unit
-       val get_current_verbosity : (unit -> int) -> unit
+       val verbosity   : int -> unit
+       val debug_level : (unit -> int) -> unit
      end
    end
 
@@ -77,7 +75,7 @@ let get_global_structures () =
   )
 
 (* The out channels are shared by all threads of the program. Hence, there is a mutex
-   per channel. File "/dev/stdout" (resp. File "/dev/stderr") is equivalent to Stdout (resp. Stderr). *)
+   per channel. `file "/dev/stdout" (resp. `file "/dev/stderr") is equivalent to `stdout (resp. `stderr). *)
 let get_out_channel log_channel =
  let (ht, ht_mutex, stdout_mutex, stderr_mutex) = get_global_structures () in
  let out_channel_and_mutex_of_filename fname =
@@ -92,39 +90,41 @@ let get_out_channel log_channel =
        end)
  in
  match log_channel with
-  | Stdout -> (stdout, stdout_mutex)
-  | Stderr -> (stderr, stderr_mutex)
-  | File fname -> Mutex.apply_with_mutex ht_mutex out_channel_and_mutex_of_filename fname
+  | `stdout -> (stdout, stdout_mutex)
+  | `stderr -> (stderr, stderr_mutex)
+  | `file fname -> Mutex.apply_with_mutex ht_mutex out_channel_and_mutex_of_filename fname
 
 
 module Make
  (Tuning:sig
-     val threshold   : int
-     val get_current_verbosity : unit -> int
-     val log_channel     : log_channel
-     val synchronized    : bool
+     val verbosity    : int
+     val debug_level  : unit -> int
+     val log_channel  : log_channel
+     val synchronized : bool
    end) : Result =
  struct
 
   (** We redefine Tuning in order to provide it a modifiable state: *)
   module Tuning = struct
    module Variable = Stateful_modules.Thread_shared_variable
-   module Threshold             = Variable (struct type t = int end)
-   module Get_current_verbosity = Variable (struct type t = unit -> int end)
+   module Verbosity = Variable (struct type t = int end)
+   module Debug_level = Variable (struct type t = unit -> int end)
    let () = begin (* Variables initialization: *)
-     Threshold.set Tuning.threshold;
-     Get_current_verbosity.set Tuning.get_current_verbosity;
+     Verbosity.set Tuning.verbosity;
+     Debug_level.set Tuning.debug_level;
    end
 
-   let threshold = Threshold.get
-   let get_current_verbosity () = Get_current_verbosity.get () ()
-   let is_debug_enabled () = (get_current_verbosity ()) >= (threshold ())
+   let verbosity = Verbosity.get
+   let debug_level () = Debug_level.get () ()
+   let is_log_enabled ?v () = match v with
+    | None   -> (debug_level ()) >= (verbosity ())
+    | Some v -> (debug_level ()) >= v 
    let log_channel  = Tuning.log_channel
    let synchronized = Tuning.synchronized
 
    module Set = struct
-    let threshold = Threshold.set
-    let get_current_verbosity = Get_current_verbosity.set
+    let verbosity = Verbosity.set
+    let debug_level = Debug_level.set
    end
 
   end (* Tuning redefinition. *)
@@ -138,9 +138,9 @@ module Make
   (* Take a format string and either use it for Printf.printf, or use it
      for a dummy printf-like function which does nothing, according to
      whether we're in debug mode or not: *)
-  let printf_unsynchronized ?(force=false) ~banner frmt =
+  let printf_unsynchronized ?v ?(force=false) ~banner frmt =
     Obj.magic
-      (if force || (Tuning.is_debug_enabled ()) then
+      (if force || (Tuning.is_log_enabled ?v ()) then
        begin
          (match banner with
            | false -> ()
@@ -156,32 +156,32 @@ module Make
       else
         Printf.ifprintf out_channel frmt)
 
-  let printf ?(force=false) ?(banner=true) frmt =
+  let printf ?v ?(force=false) ?(banner=true) frmt =
    if not Tuning.synchronized
-    then printf_unsynchronized ~force ~banner frmt
-    else apply_with_mutex (printf_unsynchronized ~force ~banner) frmt
+    then printf_unsynchronized ?v ~force ~banner frmt
+    else apply_with_mutex (printf_unsynchronized ?v ~force ~banner) frmt
 
-  let print_exn ?force ?banner e =
-   printf ?force ?banner "%s\n" (Printexc.to_string e)
+  let print_exn ?v ?force ?banner e =
+   printf ?v ?force ?banner "%s\n" (Printexc.to_string e)
 
   (* Here Obj.magic just avoids a warning "Warning X: this argument will not be used by the function.".
      For a misunderstood reason, we must define and call the function printf_nobanner into Obj.magic.
      Otherwise the banner is always printed... *)
-  let printf_nobanner ?(force=false) = printf ~force ~banner:false
-  let print_string  ?(force=false) x  = (Obj.magic (printf_nobanner ~force (format_of_string "%s"))) x
-  let print_int     ?(force=false) x  = (Obj.magic (printf_nobanner ~force (format_of_string "%d"))) x
-  let print_float   ?(force=false) x  = (Obj.magic (printf_nobanner ~force (format_of_string "%f"))) x
-  let print_endline ?(force=false) x  = (Obj.magic (printf_nobanner ~force (format_of_string "%s\n"))) x
-  let print_newline ?(force=false) () = (Obj.magic (printf_nobanner ~force (format_of_string "\n")))
+  let printf_nobanner ?v ?(force=false) = printf ?v ~force ~banner:false
+  let print_string  ?v ?(force=false) x  = (Obj.magic (printf_nobanner ?v ~force (format_of_string "%s"))) x
+  let print_int     ?v ?(force=false) x  = (Obj.magic (printf_nobanner ?v ~force (format_of_string "%d"))) x
+  let print_float   ?v ?(force=false) x  = (Obj.magic (printf_nobanner ?v ~force (format_of_string "%f"))) x
+  let print_endline ?v ?(force=false) x  = (Obj.magic (printf_nobanner ?v ~force (format_of_string "%s\n"))) x
+  let print_newline ?v ?(force=false) () = (Obj.magic (printf_nobanner ?v ~force (format_of_string "\n")))
 
 end
 
 module Make_simple (Tuning:sig val is_log_enabled : unit -> bool end) =
  Make
  (struct
-     let threshold = 1
-     let get_current_verbosity () = if Tuning.is_log_enabled () then 1 else 0
-     let log_channel     = Stderr
+     let verbosity = 1
+     let debug_level () = if Tuning.is_log_enabled () then 1 else 0
+     let log_channel     = `stderr
      let synchronized    = true
    end)
 
--- BASE/log_builder.mli
+++ BASE/log_builder.mli
@@ -22,7 +22,7 @@
 (** Facility for tracing program activities, specially using threads.
     Essentially, this module provides a function [Log.printf] printing your
     messages only when the current debugging level of your application rises
-    above a fixed threshold of verbosity. Two ore more modules may be built
+    above ([>=]) the verbosity. Two ore more modules may be built
     on the same {!Log_builder.log_channel}. Actually:
     - an internal global hash table register the associations [(file,out_channel)]
     - a mutex per out_channel is created in order to preserve atomicity of each printing.
@@ -30,29 +30,30 @@
 {b Example}:
 {[(* Define your log modules: *)
 module Log1 = Log_builder.Make (struct
-  let threshold = 1                                   (* below this limit, do nothing *)
-  let get_current_verbosity () = ...                  (* explain here how to read the current level of verbosity *)
-  let log_channel = Log_builder.File "/tmp/mylog"     (* put messages in this file *)
-  let synchronized = true                             (* using threads *)
+  let debug_level () = ...                (* explain here where to get the current value of the debugging level; 
+                                             this value must be greater or equal to the verbosity, otherwise do nothing *)
+  let verbosity = 1                       (* the default value of verbosity for printing functions *)
+  let log_channel = `stderr               (* put messages here *)
+  let synchronized = true                 (* using threads *)
  end);;
 
 module Log2 = Log_builder.Make (struct
-  let threshold = 2
-  let get_current_verbosity () = ...
-  let log_channel = Log_builder.File "/tmp/mylog"     (* share the same channel of Log1 *)
-  let synchronized = true
+  let debug_level () = ...
+  let verbosity = 2                       
+  let log_channel = `stderr               (* share the same channel of Log1 *)
+  let synchronized = true                 
  end);;
 
 (* Put calls somewhere in your code : *)
 ...
-Log1.printf "%s\n" value;   (* really printed when verbosity level >= 1 *)
+Log1.printf "%s\n" value;   (* really printed when debug level >= 1 *)
 ...
-Log2.printf "%s\n" value;   (* really printed when verbosity level >= 2 *)
+Log2.printf "%s\n" value;   (* really printed when debug level >= 2 *)
 ...
 ]}
 *)
 
-type log_channel = Stdout | Stderr | File of string
+type log_channel = [ `stdout | `stderr | `file of string ]
 
 module type Result = sig
   (**  The banner is a complement prepended to your message. It contains
@@ -67,23 +68,23 @@ toplevel 13920 [thread 0]: The result is 42
 The result is 42
    : unit = ()
 ]}*)
-  val printf : ?force:bool -> ?banner:bool -> (('a, out_channel, unit) format) -> 'a
-  val print_exn     : ?force:bool -> ?banner:bool -> exn -> unit
-  val print_string  : ?force:bool -> string -> unit
-  val print_int     : ?force:bool -> int -> unit
-  val print_float   : ?force:bool -> float -> unit
-  val print_newline : ?force:bool -> unit -> unit
-  val print_endline : ?force:bool -> string -> unit
+  val printf        : ?v:int -> ?force:bool -> ?banner:bool -> (('a, out_channel, unit) format) -> 'a
+  val print_exn     : ?v:int -> ?force:bool -> ?banner:bool -> exn -> unit
+  val print_string  : ?v:int -> ?force:bool -> string -> unit
+  val print_int     : ?v:int -> ?force:bool -> int -> unit
+  val print_float   : ?v:int -> ?force:bool -> float -> unit
+  val print_newline : ?v:int -> ?force:bool -> unit -> unit
+  val print_endline : ?v:int -> ?force:bool -> string -> unit
 
   module Tuning:sig
-     val threshold             : unit -> int
-     val get_current_verbosity : unit -> int
-     val is_debug_enabled      : unit -> bool
-     val log_channel           : log_channel
-     val synchronized          : bool
+     val verbosity      : unit -> int
+     val debug_level    : unit -> int
+     val is_log_enabled : ?v:int -> unit -> bool
+     val log_channel    : log_channel
+     val synchronized   : bool
      module Set : sig
-       val threshold             : int -> unit
-       val get_current_verbosity : (unit -> int) -> unit
+       val verbosity : int -> unit
+       val debug_level : (unit -> int) -> unit
      end
    end
 
@@ -101,10 +102,10 @@ The result is 42
 module Make :
   functor
     (Tuning : sig
-           val threshold             : int
-           val get_current_verbosity : unit -> int
-           val log_channel           : log_channel
-           val synchronized          : bool
+           val verbosity    : int
+           val debug_level  : unit -> int
+           val log_channel  : log_channel
+           val synchronized : bool
          end) -> Result
 
 (** {2 Simplified construction} *)
